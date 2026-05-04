5d7042ff492b4a85b68b3abe03c95367f78835f9
stackotter <stackotter@stackotter.dev>
2026-05-04 12:48:17 +1000

    Android: Allow devs to configure required permissions

--- Sources/SwiftBundler/Configuration/AndroidConfiguration.swift
+++ Sources/SwiftBundler/Configuration/AndroidConfiguration.swift
@@ -25,4 +25,53 @@ struct AndroidConfiguration: Codable, Sendable {
   ///
   /// [versioning]: https://developer.android.com/studio/publish/versioning
   var versionCode: Int?
+
+  /// The app's required permissions.
+  ///
+  /// Permissions take the form 'android.permission.FOO'. If any entries is the array don't
+  /// contain a period, then Swift Bundler will prepend 'android.permission.'
+  /// automatically. See the [Android Manifest.permission documentation][permissions]
+  /// for a full list of supported permissions.
+  ///
+  /// Permissions can alternatively be specified as TOML tables with a 'name' field, and
+  /// an optional 'max_sdk_version' field containing the maximum SDK version at which the
+  /// app requires the permission.
+  ///
+  /// [permissions]: https://developer.android.com/reference/android/Manifest.permission
+  var permissions: [Permission]?
+
+  /// An Android manifest permission request.
+  struct Permission: Codable, TriviallyFlattenable {
+    var name: String
+    var maxSDKVersion: Int?
+
+    enum CodingKeys: String, CodingKey {
+      case name
+      case maxSDKVersion
+    }
+
+    init(from decoder: any Decoder) throws {
+      do {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        if container.contains(.maxSDKVersion) {
+          maxSDKVersion = try container.decode(Int.self, forKey: .maxSDKVersion)
+        }
+      } catch {
+        let container = try decoder.singleValueContainer()
+        name = try container.decode(String.self)
+      }
+    }
+
+    func encode(to encoder: any Encoder) throws {
+      if let maxSDKVersion {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(maxSDKVersion, forKey: .maxSDKVersion)
+      } else {
+        var container = encoder.singleValueContainer()
+        try container.encode(name)
+      }
+    }
+  }
 }
